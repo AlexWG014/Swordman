8857b585d05bd35b3e9b2045f2ff08e7f2dea4cb
AlexWG014 <94621277+AlexWG014@users.noreply.github.com>
2024-02-25 12:49:08 +0100

    Update editar_usuario.php

--- editar_usuario.php
+++ editar_usuario.php
@@ -97,10 +97,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <input type="hidden" name="id" value="<?php echo isset($fila["id"]) ? $fila["id"] : ''; ?>">
 
-        <input type="submit" value="Actualizar">
-        <button onclick="window.location.reload();">Refrescar</button>
         </form>
 
+                <input type="submit" value="Actualizar">
+        <button onclick="window.location.reload();">Refrescar</button>
         <p>Prefieres volver? <a href="consulta.php">Volver a la consulta</a></p>
     </div>
 </body>
