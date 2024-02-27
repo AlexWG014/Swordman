62b3e20676987c82e5af514adb3c9975bf79cb3b
AlexWG014 <94621277+AlexWG014@users.noreply.github.com>
2024-02-27 19:59:48 +0100

    Update quienes_somos.php

--- quienes_somos.php
+++ quienes_somos.php
@@ -6,8 +6,8 @@ include 'conectar.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Quiénes Somos</title>
-    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
